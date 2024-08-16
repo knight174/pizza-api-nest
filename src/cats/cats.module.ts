29ea11a0cdb819b8b68ec10e3b0d0e3508afbe53
pinchao <cpc4253@163.com>
2024-08-16 15:30:59 +0800

    feat: 全局模块与服务导出

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
 
+@Global()
 @Module({
   controllers: [CatsController],
   providers: [CatsService],
+  exports: [CatsService], // 成为共享模块。任何导入了 Cats Module 的模块都可以使用 CatsService
 })
 export class CatsModule {}
