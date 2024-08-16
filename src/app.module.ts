b51f01e6464f47479b0fc6187c4e1fe91d8b0841
pinchao <cpc4253@163.com>
2024-08-16 15:43:51 +0800

    feat: add logger-middleware for cats

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({
+      path: 'cats', // 只对 cats 路由启用中间件
+      method: RequestMethod.GET, // 只对 GET 请求启用中间件
+    });
+  }
+}
