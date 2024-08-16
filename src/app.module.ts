8bc1ae95fae2748829c3c4111e669fc50c4da2ae
pinchao <cpc4253@163.com>
2024-08-16 15:44:58 +0800

    feat: 使用路由控制器喂给中间件消费者

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { CatsController } from './cats/cats.controller';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
@@ -16,9 +12,6 @@ import { LoggerMiddleware } from './common/middleware/logger.middleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes({
-      path: 'cats', // 只对 cats 路由启用中间件
-      method: RequestMethod.GET, // 只对 GET 请求启用中间件
-    });
+    consumer.apply(LoggerMiddleware).forRoutes(CatsController);
   }
 }
