b411d0537ff5327abb72210a5708ff823e23b6b9
pinchao <cpc4253@163.com>
2024-08-18 21:44:03 +0800

    feat: add prisma module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { UsersModule } from './users/users.module';
 import { PizzasModule } from './pizzas/pizzas.module';
 import { CartsModule } from './carts/carts.module';
 import { OrdersModule } from './orders/orders.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UsersModule, PizzasModule, CartsModule, OrdersModule],
+  imports: [UsersModule, PizzasModule, CartsModule, OrdersModule, PrismaModule],
 })
 export class AppModule {}
