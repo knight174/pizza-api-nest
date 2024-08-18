5cab59ce93a90799e53a0b1b03c670b45e05ab93
pinchao <cpc4253@163.com>
2024-08-18 21:38:02 +0800

    docs: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Pizza API')
+    .setDescription('The Pizza API description')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
     prefix: 'api/v',
   });
+
   await app.listen(3000);
 }
 bootstrap();
