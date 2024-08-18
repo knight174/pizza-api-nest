60050e2be2a430ee9f81ba43530cfb9771ba135c
pinchao <cpc4253@163.com>
2024-08-18 22:44:14 +0800

    docs: 更新swagger 相应类型

--- src/pizzas/entities/pizza.entity.ts
+++ src/pizzas/entities/pizza.entity.ts
@@ -1,1 +1,70 @@
-export class Pizza {}
+import { Pizza } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+import { Decimal } from '@prisma/client/runtime/library';
+
+export class PizzaEntity implements Pizza {
+  @ApiProperty({
+    description: 'Unique identifier for the pizza',
+    example: 'e0169a2c-1509-4050-9195-1fa30062ff97',
+  })
+  id: string;
+
+  @ApiProperty({
+    description: 'Name of the pizza',
+    example: 'Pizza 1',
+  })
+  name: string;
+
+  @ApiProperty({
+    description: 'Price of the pizza',
+    example: 10.0,
+  })
+  price: Decimal;
+
+  @ApiProperty({
+    description: 'Discount rate applied to the pizza price',
+    example: 0.6,
+  })
+  discount: Decimal;
+
+  @ApiProperty({
+    description: 'Number of units sold',
+    example: 10,
+  })
+  sales: number;
+
+  @ApiProperty({
+    description: 'Size of the pizza in inches',
+    example: 8,
+  })
+  size: number;
+
+  @ApiProperty({
+    description: 'Tag associated with the pizza for categorization',
+    example: 'discount',
+  })
+  tag: string;
+
+  @ApiProperty({
+    description: 'Deletion timestamp if the pizza has been soft deleted',
+    required: false,
+  })
+  deletedAt: Date | null;
+
+  @ApiProperty({
+    description: 'Timestamp of pizza creation',
+  })
+  createdAt: Date;
+
+  @ApiProperty({
+    description: 'Timestamp of the last update',
+  })
+  updatedAt: Date;
+
+  @ApiProperty({
+    description: 'URL of the pizza image',
+    example: 'https://images.unsplash.com/...',
+    required: false,
+  })
+  src: string;
+}
--- src/pizzas/pizzas.controller.ts
+++ src/pizzas/pizzas.controller.ts
@@ -11,7 +11,9 @@ import {
 import { PizzasService } from './pizzas.service';
 import { CreatePizzaDto } from './dto/create-pizza.dto';
 import { UpdatePizzaDto } from './dto/update-pizza.dto';
-import { ApiTags } from '@nestjs/swagger';
+
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { PizzaEntity } from './entities/pizza.entity';
 
 @Controller('pizzas')
 @ApiTags('pizzas')
@@ -19,27 +21,32 @@ export class PizzasController {
   constructor(private readonly pizzasService: PizzasService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: PizzaEntity })
   create(@Body() createPizzaDto: CreatePizzaDto) {
     return this.pizzasService.create(createPizzaDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: PizzaEntity, isArray: true })
   async findAll(@Query('kind') kind: string = 'all') {
     const pizzas = await this.pizzasService.findAll(kind);
     return pizzas;
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: PizzaEntity })
   findOne(@Param('id') id: string) {
     return this.pizzasService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: PizzaEntity })
   update(@Param('id') id: string, @Body() updatePizzaDto: UpdatePizzaDto) {
     return this.pizzasService.update(id, updatePizzaDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: PizzaEntity })
   remove(@Param('id') id: string) {
     return this.pizzasService.remove(id);
   }
