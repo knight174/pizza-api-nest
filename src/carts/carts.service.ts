7fbc943ca12eb1f36ec76f63d1707c174722fc7d
pinchao <cpc4253@163.com>
2024-08-20 13:29:02 +0800

    feat: 创建或添加购物车逻辑以及批量添加

--- src/carts/carts.service.ts
+++ src/carts/carts.service.ts
@@ -7,14 +7,33 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class CartsService {
   constructor(private prisma: PrismaService) {}
 
-  // 创建购物车
-  create(userId: string, createCartDto: CreateCartDto) {
-    return this.prisma.cart.create({
-      data: {
+  // 创建/添加购物车
+  async create(userId: string, createCartDto: CreateCartDto) {
+    const existingCart = await this.prisma.cart.findFirst({
+      where: {
         userId,
-        ...createCartDto,
+        pizzaId: createCartDto.pizzaId,
+        deletedAt: null,
       },
     });
+
+    if (existingCart) {
+      // 更新数量
+      return this.prisma.cart.update({
+        where: { id: existingCart.id },
+        data: { quantity: { increment: createCartDto.quantity } },
+      });
+    } else {
+      // 创建新购物车项
+      return this.prisma.cart.create({
+        data: {
+          pizzaId: createCartDto.pizzaId,
+          quantity: createCartDto.quantity,
+          selected: createCartDto.selected ?? true,
+          userId: userId,
+        },
+      });
+    }
   }
 
   // 获取用户购物车
@@ -39,13 +58,47 @@ export class CartsService {
 
   // 更新购物车
   async update(id: string, updateCartDto: UpdateCartDto) {
-    const cart = await this.prisma.cart.findUnique({ where: { id } });
+    const cart = await this.prisma.cart.findUnique({
+      where: { id, deletedAt: null },
+    });
     if (!cart) throw new NotFoundException('Cart not found');
 
     return this.prisma.cart.update({
       where: { id },
       data: updateCartDto,
     });
+  }
+
+  // 批量更新
+  async updateMultiple(ids: string[], selected: boolean) {
+    // 查找未删除的购物车项
+    const carts = await this.prisma.cart.findMany({
+      where: {
+        id: {
+          in: ids,
+        },
+        deletedAt: null,
+      },
+    });
+
+    if (carts.length > 0) {
+      // 批量更新
+      const updatedRecords = await this.prisma.cart.updateMany({
+        where: {
+          id: {
+            in: ids,
+          },
+          deletedAt: null, // 确保未删除的项
+        },
+        data: {
+          selected: selected,
+        },
+      });
+
+      return updatedRecords;
+    } else {
+      throw new NotFoundException('购物车不存在');
+    }
   }
 
   // 删除购物车
